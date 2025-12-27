6b01736d0d802f3106b1e9f793d1f44ad43cb7ef
Ryan Jensen <ryanj@cs.utah.edu>
2025-12-27 09:13:48 -0700

    improvements

--- cloud-lift-generator/script.js
+++ cloud-lift-generator/script.js
@@ -17,14 +17,10 @@ function getParams() {
 }
 
 function render() {
-  console.log("rendered");
   const { angle, height, radius } = getParams();
   const svg = getSvg(angle, height, radius);
-  const testSvg = geminiSvg();
-  console.log("svg", svg);
   const curve = document.getElementById("curve");
   curve.innerHTML = svg;
-  curve.innerHTML = testSvg;
 }
 
 function getPoint(x, y) {
@@ -38,7 +34,8 @@ function getPoint(x, y) {
 }
 
 function getPointOnAngle(fromX, fromY, angle, height) {
-  const runX = height / Math.abs(Math.tan(angle));
+  const angleInRadians = (angle * Math.PI) / 180;
+  const runX = height / Math.tan(angleInRadians);
   return getPoint(fromX + runX, fromY - height);
 }
 
@@ -49,19 +46,15 @@ function getSvg(angle, height, radius) {
 
   const p1 = getPoint(baseX, baseY);
   const p2 = getPoint(baseX + runLen, baseY);
-
-  console.log("p1", p1.str());
-  console.log("p2", p2.str());
   const p3 = getPointOnAngle(p2.x, p2.y, angle, height);
-  const midpoint = getPoint((p2.x + p3.x) / 2, (p2.y + p3.y) / 2);
-  console.log("midpoint", midpoint.str());
-  console.log("p3", p3.str());
-  const control1 = getPoint(p2.x + radius * 0.5, p2.y); // bottom curve anchor
-  const control2 = getPoint(p3.x + radius * 0.5, p3.y); // top curve anchor
-
   const p4 = getPoint(p3.x + runLen, p3.y);
 
-  // ${p3.str()} ${control2.str()} ${p4.str()}
+  console.log("p2", p2.str());
+  console.log("p3", p3.str());
+
+  const controlP2 = getPoint(p2.x + radius, p2.y);
+  const controlP3 = getPoint(p3.x - radius, p3.y);
+
   return `
     <svg
       width="500"
@@ -72,10 +65,8 @@ function getSvg(angle, height, radius) {
       <path
         d=" M ${p1.str()} 
             L ${p2.str()} 
-            C ${control1.str()}, ${midpoint.str()}, ${midpoint.str()}
-            S ${p3.str()}, ${control2.str()}
+            C ${controlP2.str()}, ${controlP3.str()}, ${p3.str()}
             L ${p4.str()}
-               
           "
         fill="none"
         stroke="black"
@@ -83,14 +74,4 @@ function getSvg(angle, height, radius) {
       />
     </svg>
   `;
-}
-
-function geminiSvg() {
-  return `<svg width="500" height="250" viewBox="0 0 500 250" xmlns="http://www.w3.org/2000/svg">
-  <path d="M 50 150 L 200 150
-           C 250 150, 300 100, 300 100
-           S 350 50, 400 50
-           L 500 50" 
-        fill="none" stroke="black" stroke-width="2" />
-</svg>`;
 }
